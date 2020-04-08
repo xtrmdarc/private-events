20abcd35b2cfe18c0eed23225c8466674e551ff7
xtrmdarc <diegoreyesco@hotmail.com>
2020-04-08 14:36:13 -0500

    fix linter issues

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,9 +4,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'valid signup information' do
     get new_user_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  'Example User',
+      post users_path, params: { user: { name: 'Example User',
                                          email: 'user@example.com',
-                                         password:              'password',
+                                         password: 'password',
                                          password_confirmation: 'password' } }
     end
   end
