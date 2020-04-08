5a505bea486afafa17be78fc200c4d61f708ebe0
bren2102 <brb.yg.21@gmail.com>
2020-04-08 14:16:44 -0500

    Fix linter issues

--- test/integration/event_creation_test.rb
+++ test/integration/event_creation_test.rb
@@ -5,14 +5,14 @@ class EventCreationTest < ActionDispatch::IntegrationTest
     @user = User.create(name: 'Example User', email: 'user@example.com',
                         password: 'foobar', password_confirmation: 'foobar')
   end
-  test "valid create event" do
+  test 'valid create event' do
     log_in_as(@user)
     get new_event_path
     assert_difference 'Event.count', 1 do
-      post events_path, params: { event: { title:  "My First event",
-                                         description: "Fun party",
-                                         date:              "04-10-2020",
-                                         location: "My place" } }
+      post events_path, params: { event: { title: 'My First event',
+                                           description: 'Fun party',
+                                           date: '04-10-2020',
+                                           location: 'My place' } }
     end
   end
 end
